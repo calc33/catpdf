c8a24506fbc6241488a4b0005b6b8aad27ced4ba
BABA Motoharu <calc33@gmail.com>
2022-08-14 09:11:08 +0900

    release.batをPgToolsのrelease.batを元に改修

--- release.bat
+++ release.bat
@@ -1,3 +1,27 @@
 @ECHO OFF
 
-XCOPY /Y /S /EXCLUDE:exclude.txt bin\Release Release
+REM 環境変数SIGNには sign.bat のパスをセットする
+SET SIGN=C:\bin\sign.bat
+
+SET DBG=bin\Debug\catpdf.exe
+SET REL=bin\Release\catpdf.exe
+IF NOT EXIST %REL% (
+  ECHO "Releaseでビルドされていません"
+  GOTO END
+)
+IF NOT EXIST %DBG% GOTO COPY
+FOR %%a IN ( %DBG% ) DO SET TDBG=%%~ta
+FOR %%b IN ( %REL% ) DO SET TREL=%%~tb
+
+IF "%TDBG%" GTR "%TREL%" (
+  ECHO "最後のビルドがDebugで行われています。Releaseでビルドしてください。"
+  GOTO END
+)
+
+:COPY
+REM MKDIR Release
+CMD /C %SIGN% bin\Release\catpdf.exe
+XCOPY /Y /S /I /EXCLUDE:exclude.txt bin\Release Release
+
+:END
+PAUSE
